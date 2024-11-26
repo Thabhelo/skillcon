ed76b72bb8fe4f3d6886da3f8cc846b9607fd38e
Thabhelo <thabhelo.duve@talladega.edu>
2024-11-26 03:39:36 -0600

    environment variables in environments/environment.ts and environment.prod.ts set up

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { environment } from '../environments/environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCgvc2gM6PIeNWXdYyzZnrVoFBnlyzpCNQ",
-  authDomain: "skillcon-5a5fc.firebaseapp.com",
-  projectId: "skillcon-5a5fc",
-  storageBucket: "skillcon-5a5fc.firebasestorage.app",
-  messagingSenderId: "659795856207",
-  appId: "1:659795856207:web:49ee581185a1b97564533a"
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
